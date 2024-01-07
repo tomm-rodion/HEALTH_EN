0618475a2bc3bc7a71be84db5f65b583a58ea70f
tomm-rodion <autovolna@gmail.com>
2024-01-08 00:00:13 +0200

    header -refactoring

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,244 @@
-import { Container } from '../Container/Container';
-import { HeaderStyled, ContainerForHeader } from './Header.styled';
-import { Navigation } from './Navigation/Navigation';
-import { UserMenu } from './UserMenu/UserMenu';
-import { AuthNav } from './AuthNav/AuthNav';
-import { useAuth } from '../../hooks/useAuth';
+// import { Container } from '../Container/Container';
+// import { HeaderStyled, ContainerForHeader } from './Header.styled';
+// import { Navigation } from './Navigation/Navigation';
+// import { UserMenu } from './UserMenu/UserMenu';
+// import { AuthNav } from './AuthNav/AuthNav';
+// import { useAuth } from '../../hooks/useAuth';
 
+// export const Header = () => {
+//   const {isLoggedIn} = useAuth();
+
+//   return (
+//     <HeaderStyled>
+//       <Container>
+//         <ContainerForHeader>
+//           <Navigation />
+//           {isLoggedIn ? <UserMenu /> : <AuthNav />}
+//         </ContainerForHeader>
+//       </Container>
+//     </HeaderStyled>
+//   );
+// };
+
+import { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectUserName, selectLoggedUser } from 'redux/Auth/auth-selectors';
+
+import {
+  Container,
+  Content,
+  LogoBox,
+  Info,
+  InfoOptions,
+  InfoBlock,
+  UserBlock,
+  IconContainer,
+  TextContainer,
+  InfoBlockName,
+  InfoBlockText,
+  WeightKg,
+  ArrowSvg,
+  EditSvg,
+  AvaImg,
+  MenuBox,
+  MenuImg,
+  Unauthorized,
+  DefaultLink,
+  ActiveLink,
+} from './Header.styled';
+
+import waight from '../../images/icons-emoji/Waight image.png';
+
+import loseFatMen from '../../images/icons-emoji/Lose fat image men.png';
+import loseFatGirl from '../../images/icons-emoji/Lose fat image girl.png';
+import maintakeMen from '../../images/icons-emoji/Maintake image men.png';
+import maintakeGirl from '../../images/icons-emoji/Maintake image girl.png';
+import muscle from '../../images/icons-emoji/Gain muscle.png';
+
+import arrowDown from '../../images/icons-linear/arrow-down.svg';
+import edit from '../../images/icons-linear/edit-2.svg';
+import menu from '../../images/icons-linear/menu.svg';
+import menuOpened from '../../images/icons-linear/menu.svg';
+import avatar from '../../images/icons-linear/profile-circle.svg';
+
+// import TargetModal from '../HeaderModals/TargetModal';
+// import WeightModal from '../HeaderModals/WeightModal';
+// import ProfileModal from '../HeaderModals/ProfileModal';
+// import MenuModal from '../HeaderModals/MenuModal';
 
 export const Header = () => {
-  const {isLoggedIn} = useAuth();
+  const [showModalTarget, setShowModalTarget] = useState(false);
+  const [showModalWeight, setShowModalWeight] = useState(false);
+  const [showModalProfile, setShowModalProfile] = useState(false);
+  const [showModalMenu, setShowModalMenu] = useState(false);
 
+  const location = useLocation();
+  const [currentURL, setCurrentURL] = useState(location.pathname);
 
-  return (
-    <HeaderStyled>
-      <Container>
-        <ContainerForHeader>
-          <Navigation />
-          {isLoggedIn ? <UserMenu /> : <AuthNav />}
-        </ContainerForHeader>
-      </Container>
-    </HeaderStyled>
+  const user = useSelector(selectUserName);
+  const isLoggedIn = useSelector(selectLoggedUser);
+
+  let goalIcon;
+
+  switch (user.gender) {
+    case 'Female':
+      switch (user.goal) {
+        case 'Lose fat':
+          goalIcon = loseFatGirl;
+          break;
+
+        case 'Maintain':
+          goalIcon = maintakeGirl;
+          break;
+
+        default:
+          goalIcon = muscle;
+      }
+      break;
+
+    case 'Male':
+      switch (user.goal) {
+        case 'Lose fat':
+          goalIcon = loseFatMen;
+          break;
+
+        case 'Maintain':
+          goalIcon = maintakeMen;
+          break;
+
+        default:
+          goalIcon = muscle;
+      }
+      break;
+
+    default:
+      goalIcon = muscle;
+  }
+
+  const toggleModalTarget = () => {
+    setShowModalTarget(showModalTarget => !showModalTarget);
+  };
+
+  const toggleModalWeight = () => {
+    setShowModalWeight(showModalWeight => !showModalWeight);
+  };
+
+  const toggleModalProfile = () => {
+    setShowModalProfile(showModalProfile => !showModalProfile);
+  };
+
+  const toggleModalMenu = () => {
+    setShowModalMenu(showModalMenu => !showModalMenu);
+  };
+
+  useEffect(() => {
+    // Update currentURL when the URL changes
+    setCurrentURL(location.pathname);
+  }, [location]);
+
+  return isLoggedIn ? (
+    <Container>
+      <Content>
+        <LogoBox>
+          <Link to={'/main'}>HealthyHub</Link>
+          <MenuBox onClick={toggleModalMenu}>
+            {showModalMenu ? (
+              <MenuImg src={menuOpened} alt="menu" />
+            ) : (
+              <MenuImg src={menu} alt="menu" />
+            )}
+            {/* {showModalMenu && <MenuModal onCloseModal={toggleModalMenu} />} */}
+          </MenuBox>
+        </LogoBox>
+        <Info>
+          <InfoOptions>
+            <InfoBlock onClick={toggleModalTarget}>
+              <IconContainer>
+                <img src={goalIcon} alt="current goal" width={28} />
+              </IconContainer>
+              <TextContainer>
+                <InfoBlockName>Goal</InfoBlockName>
+                <InfoBlockText>
+                  {user.goal || 'No goal yet'}
+                  <ArrowSvg
+                    src={arrowDown}
+                    alt="arrow down"
+                    style={{
+                      transform: showModalTarget
+                        ? 'rotate(180deg)'
+                        : 'rotate(0deg)',
+                    }}
+                  />
+                </InfoBlockText>
+              </TextContainer>
+              {/* {showModalTarget && (
+                <TargetModal onCloseModal={toggleModalTarget} />
+              )} */}
+            </InfoBlock>
+            <InfoBlock onClick={toggleModalWeight}>
+              <IconContainer>
+                <img src={waight} alt="weight" width={28} />
+              </IconContainer>
+              <TextContainer>
+                <InfoBlockName>Weight</InfoBlockName>
+                <InfoBlockText>
+                  {user.weight || 0}
+                  <WeightKg>kg</WeightKg>
+                  <EditSvg src={edit} alt="edit" />
+                </InfoBlockText>
+              </TextContainer>
+              {/* {showModalWeight && (
+                <WeightModal onCloseModal={toggleModalWeight} />
+              )} */}
+            </InfoBlock>
+          </InfoOptions>
+          <UserBlock onClick={toggleModalProfile}>
+            {user.name || 'User'}
+            <AvaImg src={user.avatarURL || avatar} alt="avatar" />
+            <ArrowSvg
+              src={arrowDown}
+              alt="arrow down"
+              style={{
+                transform: showModalProfile ? 'rotate(180deg)' : 'rotate(0deg)',
+              }}
+            />
+            {/* {showModalProfile && (
+              <ProfileModal onCloseModal={toggleModalProfile} />
+            )} */}
+          </UserBlock>
+        </Info>
+      </Content>
+    </Container>
+  ) : (
+    <Container>
+      <Content>
+        <LogoBox>
+          <Link to={'/'}>HealthyHub</Link>
+        </LogoBox>
+        {currentURL.endsWith('/signin') ? (
+          <Unauthorized>
+            <ActiveLink to={'/signin'}>Sign in</ActiveLink>
+            <p> / </p>
+            <DefaultLink to={'/signup'}>Sign up</DefaultLink>
+            <img src={avatar} alt="weight" width={28} />
+          </Unauthorized>
+        ) : currentURL.endsWith('/signup') ? (
+          <Unauthorized>
+            <DefaultLink to={'/signin'}>Sign in</DefaultLink>
+            <p> / </p>
+            <ActiveLink to={'/signup'}>Sign up</ActiveLink>
+            <img src={avatar} alt="weight" width={28} />
+          </Unauthorized>
+        ) : (
+          <Unauthorized>
+            <DefaultLink to={'/signin'}>Sign in</DefaultLink>
+            <p> / </p>
+            <DefaultLink to={'/signup'}>Sign up</DefaultLink>
+            <img src={avatar} alt="weight" width={28} />
+          </Unauthorized>
+        )}
+      </Content>
+    </Container>
   );
 };
--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -1,17 +1,214 @@
-import { styled } from 'styled-components';
+// import { styled } from 'styled-components';
 
-export const HeaderStyled = styled.header`
-  background-color: ${p => p.theme.color.primary_black_2};
+// export const HeaderStyled = styled.header`
+//   background-color: ${p => p.theme.color.primary_black_2};
+// `;
+
+// export const ContainerForHeader = styled.div`
+//   display: flex;
+//   padding: 19px 0;
+//   width: 100%;
+//   //position for the "accordion" of the mobile menu
+//   position: relative;
+
+//   @media screen and (min-width: 834px) {
+//     padding: 34px 0;
+//   }
+// `;
+
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const Container = styled.div`
+  background-color: ${props => props.theme.colors.black2};
+
+  @media screen and (${props => props.theme.mq.mobile}) {
+    padding-top: 18px;
+    padding-bottom: 18px;
+  }
+  @media screen and (${props => props.theme.mq.tablet}) {
+    padding-top: 26px;
+    padding-bottom: 26px;
+  }
 `;
 
-export const ContainerForHeader = styled.div`
+export const Content = styled.div`
   display: flex;
-  padding: 19px 0;
-  width: 100%;
-  //position for the "accordion" of the mobile menu
-  position: relative;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 auto;
 
-  @media screen and (min-width: 834px) {
-    padding: 34px 0;
+  width: 320px;
+  padding: 0 10px;
+
+  @media screen and (${props => props.theme.mq.tablet}) {
+    width: 834px;
+    padding: 0 27px;
   }
+
+  @media screen and (${props => props.theme.mq.desktop}) {
+    width: 1440px;
+    padding: 0 34px;
+  }
+`;
+
+export const LogoBox = styled.div`
+  color: ${props => props.theme.colors.gray};
+  font-size: ${props => props.theme.fontSizes.l};
+  font-weight: ${props => props.theme.fontWeights.bold};
+
+  @media screen and (${props => props.theme.mq.mobile}) {
+    font-size: ${props => props.theme.fontSizes.s};
+    display: flex;
+    align-items: center;
+  }
+`;
+
+export const MenuBox = styled.div`
+  margin-left: 8px;
+  @media screen and (${props => props.theme.mq.tablet}) {
+    display: none;
+  }
+`;
+
+export const MenuImg = styled.img`
+  width: 16px;
+`;
+
+export const Info = styled.div`
+  margin: 0;
+  display: flex;
+  justify-content: space-between;
+
+  @media screen and (${props => props.theme.mq.tablet}) {
+    width: 513px;
+  }
+  @media screen and (${props => props.theme.mq.desktop}) {
+    width: 604px;
+  }
+`;
+
+export const InfoOptions = styled.div`
+  display: flex;
+`;
+
+export const InfoBlock = styled.div`
+  background-color: inherit;
+  border: none;
+  cursor: pointer;
+
+  @media screen and (${props => props.theme.mq.mobile}) {
+    display: none;
+  }
+
+  @media screen and (${props => props.theme.mq.tablet}) {
+    display: flex;
+    &:nth-child(1) {
+      margin-right: 40px;
+    }
+  }
+
+  @media screen and (${props => props.theme.mq.desktop}) {
+    display: flex;
+    &:nth-child(1) {
+      margin-right: 80px;
+    }
+  }
+`;
+
+export const IconContainer = styled.div`
+  padding: 10px;
+  background-color: rgba(255, 255, 255, 0.01);
+  border: 1px solid ${props => props.theme.colors.gray1};
+  border-radius: ${div => div.theme.radii.normal};
+  width: 48px;
+  height: 48px;
+  margin-right: 12px;
+`;
+
+export const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  padding: 3px 0 3px 0;
+`;
+
+export const InfoBlockName = styled.p`
+  color: ${p => p.theme.colors.white};
+  font-size: ${p => p.theme.fontSizes.xs};
+  font-weight: ${p => p.theme.fontWeights.semiBold};
+`;
+
+export const InfoBlockText = styled.p`
+  color: ${p => p.theme.colors.white};
+  font-size: ${p => p.theme.fontSizes.xs};
+  font-weight: ${p => p.theme.fontWeights.medium};
+  display: flex;
+  align-items: center;
+`;
+
+export const ArrowSvg = styled.img`
+  width: 14px;
+  margin-left: 12px;
+`;
+
+export const WeightKg = styled.span`
+  color: ${p => p.theme.colors.gray};
+  font-size: ${p => p.theme.fontSizes.xs};
+  font-weight: ${p => p.theme.fontWeights.regular};
+  margin-left: 4px;
+  margin-right: 6px;
+`;
+
+export const EditSvg = styled.img`
+  width: 16px;
+`;
+
+export const UserBlock = styled.div`
+  color: ${p => p.theme.colors.white};
+  font-size: ${p => p.theme.fontSizes.xs};
+  font-weight: ${p => p.theme.fontWeights.regular};
+  display: flex;
+  align-items: center;
+`;
+
+export const AvaImg = styled.img`
+  width: 28px;
+  border-radius: 50%;
+  margin-left: 6px;
+  margin-right: -8px;
+  @media screen and (${props => props.theme.mq.mobile}) {
+    width: 24px;
+  }
+  @media screen and (${props => props.theme.mq.tablet}) {
+    width: 28px;
+  }
+`;
+
+export const Unauthorized = styled.div`
+  color: ${p => p.theme.colors.white};
+  font-size: ${p => p.theme.fontSizes.xs};
+  font-weight: ${p => p.theme.fontWeights.regular};
+  display: flex;
+  align-items: center;
+  gap: 6px;
+  @media screen and (${props => props.theme.mq.mobile}) {
+    margin-right: 10px;
+  }
+
+  @media screen and (${props => props.theme.mq.tablet}) {
+    margin-right: 27px;
+  }
+
+  @media screen and (${props => props.theme.mq.desktop}) {
+    margin-right: 34px;
+  }
+`;
+
+export const DefaultLink = styled(Link)`
+  color: ${p => p.theme.colors.white};
+`;
+
+export const ActiveLink = styled(Link)`
+  color: ${p => p.theme.colors.greenLite};
 `;
